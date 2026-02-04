8760f79bde78240107288ee4c701eb122e159c4f
Damian <damian.radomski9908@gmail.com>
2026-02-04 22:42:49 +0100

    next try fix

--- vue-homeorganizer/src/layouts/MainLayout.vue
+++ vue-homeorganizer/src/layouts/MainLayout.vue
@@ -23,7 +23,7 @@
     </header>
 
     <div class="layout-body">
-      <SideBar />
+      <Sidebar />
 
       <main class="content-area">
         <router-view />
@@ -36,7 +36,7 @@
 import { ref, onMounted, onUnmounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-import SideBar from '@/components/SideBar.vue'
+import Sidebar from '@/components/SideBar.vue'
 
 const router = useRouter()
 const authStore = useAuthStore()
